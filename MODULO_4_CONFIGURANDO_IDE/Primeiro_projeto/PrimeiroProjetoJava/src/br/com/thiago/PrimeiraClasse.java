647788f690ba90c26203c99deabe9350407c64a2
Thiago <tlucenalive@gmail.com>
2023-11-14 08:58:51 -0300

    Modulo 4 - Configuração da IDE

---
+++ MODULO_4_CONFIGURANDO_IDE/Primeiro_projeto/PrimeiroProjetoJava/src/br/com/thiago/PrimeiraClasse.java
@@ -0,0 +1,11 @@
+package br.com.thiago;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		String ambiente = args[0];
+		if (ambiente.equals("DEV")) {
+			System.out.print(ambiente);
+		}
+	}
+}
